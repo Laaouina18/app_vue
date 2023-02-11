fecd66ff0f898fe98a5bfc8105114adaab9fa676
Laaouina18 <laaouinanouhaila2019@gmail.com>
2023-02-11 16:53:42 +0100

    vue js

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+import{ref} from "vue"; 
+
+import Thetodos from "./components/Thetodos.vue";
+
+
+
+var name=ref("mhdi");
+
+var todos=ref([
+  {
+    title:"HI HI",
+    is_done:true
+  },
+  {
+    title:"NOUHA",
+    is_done:false
+  },
+  {
+    title:"LAAOUINA",
+    is_done:false
+  },
+])
+const addTodo=()=>{
+  todos.value.push({
+    title:name.value,
+    is_done:false
+  })
+}
+
+</script>
+
+<template>
+<div class="app">
+  <div class="content">
+    <Thetodos :todos="todos"/>
+    <p>{{name}}</p>
+
+  </div>
+  <div class="zone">
+<input v-model="name" type="text" name="tod" class="input1">
+<button class="btn" @click="addTodo">add</button>
+  </div>
+</div>
+</template>
+
+<style scoped>
+*{
+  margin:0;
+  padding:0;
+
+}
+.app{
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height:100vh;
+
+}
+.btn{
+  border-radius: 5px;
+}
+</style>
---
+++ src/components/Thetodos.vue
@@ -0,0 +1,59 @@
+<script setup>
+import {ref} from "vue";
+
+
+defineProps({
+    todos:[]
+})
+
+
+// const changeTodo = (todo)=>{
+//     todo.value.push({
+//     is_done:true
+//   })
+// }
+
+</script>
+<template>
+    <ul>
+    <h3>To Do</h3> 
+
+    
+  
+  <div v-for="todo in todos" :key="todo.value" class="todo" >
+  <div v-if="todo.is_done==false" class="done">
+    <input  type="checkbox" >
+   {{  todo.title}}
+   {{ todo.is_done }}
+</div>
+
+   
+</div>
+<hr>
+<h3>Done</h3>
+
+
+  <div v-for="todo in todos" :key="todo.value" class="todo">
+  <div v-if="todo.is_done==true" class="done">
+   
+   {{  todo.title}}
+   {{ todo.is_done }}
+</div>
+</div>
+</ul>
+</template>
+<style>
+.todo{
+    border:1px solid;
+    background-color: rgb(85, 171, 85);
+    color:white;
+    
+}
+.done{
+    padding:10px 22px;
+    font-family:Arial, Helvetica, sans-serif;
+}
+h3{
+    font: optional;
+}
+</style>
